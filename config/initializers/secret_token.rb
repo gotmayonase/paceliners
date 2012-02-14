8f29afc69fc5fab5a07f452e7f73813b056a4546
Mike Mayo <mike@twinenginelabs.com>
2012-02-14 00:40:15 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paceliners::Application.config.secret_token = 'db005f76bec931a0803b035fb58e1dcdccc78451f8b24f655d34cbbb04db5d3aa96c003370422b4e40d8fe8afcb63075f6e546649a4c3efcc023a300839493f9'
